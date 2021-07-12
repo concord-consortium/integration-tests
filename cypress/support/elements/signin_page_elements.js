c115960a797ef756a5d41116793c66e416a81476
Srivardhan <sri.sun@tscube.com>
2021-07-12 17:30:18 +0530

    reverting back previous changes as they work in qa env

--- cypress/support/elements/signin_page_elements.js
+++ cypress/support/elements/signin_page_elements.js
@@ -1,13 +1,10 @@
 const signinPageElements = {
   // Sign in form and page elements
   FORM : '.signup-form',
-  USERNAME_FIELD : 'div.login-default-modal-content .signup-form input[type=\"text\"]',
-  PASSWORD_FIELD : 'div.login-default-modal-content .signup-form input[type=\"password\"]',
-  LOGIN_AFTER_REGISTER_USERNAME_FIELD: '.signup-form input[type=\"text\"]',
-  LOGIN_AFTER_REGISTER_PASSWORD_FIELD: '.signup-form input[type=\"password\"]',
+  USERNAME_FIELD : '.signup-form input[type=\"text\"]',
+  PASSWORD_FIELD : '.signup-form input[type=\"password\"]',
   FORGOT_LINK : '.signup-form .submit-button-container a',
-  LOGIN_BUTTON : 'div.login-default-modal-content .signup-form .submit-btn',
-  LOGIN_AFTER_REGISTER_LOGIN_BUTTON: '.signup-form .submit-btn',
+  LOGIN_BUTTON : '.signup-form .submit-btn',
   SIGNUP_LINK : '.signup-form footer a',
   CLOSE_BUTTON : '.portal-pages-close',
   CLOSE_BUTTON_II : '#login-default-modal .portal-pages-close',
