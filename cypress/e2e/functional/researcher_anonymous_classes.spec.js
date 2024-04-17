16209d048949e9dec2076cc854aa2ac425156a31
Saravanan Udayakumar <saravanan.udayakumar@tscube.com>
2024-04-17 19:46:59 +0530

    Researcher Anonymous Class Automation

---
+++ cypress/e2e/functional/researcher_anonymous_classes.spec.js
@@ -0,0 +1,149 @@
+import * as c from '../../support/constants.js'
+import * as researchClassesHelper from "../../support/helpers/researcherClassesHelper.js"
+import {ResearcherClassesElements as researchClasses} from "../../support/elements/researcher_anonymous_classes_elements";
+
+context("Researcher classes side bar, filters, resutls table", () => {
+
+  before(function() {
+    cy.visit(c.LEARN_PORTAL_BASE_URL); // Visit LEARN Portal home page
+    cy.login(c.TEACHER3_USERNAME, c.TEACHER3_PASSWORD); // Login as teacher user
+  });
+
+  after(function() {
+    cy.logout();
+    cy.clearAllCookies();
+  });
+
+  it("Verify research project in sidebar", () => {
+    researchClassesHelper.expandResearchProjects(); 
+    researchClassesHelper.getResearchProjectsOpen().should("exist");
+  });
+  it("Verify research classes landing page", () => {
+    researchClassesHelper.verifyProjectsDisplayed();
+    researchClassesHelper.clickProject();
+    researchClassesHelper.getResearchClassLandingPage().should("exist");
+    researchClasses.getHeader().contains("Research Classes: Test Project For Researcher");
+    researchClasses.verifyCohortsHeader();
+    researchClasses.verifyTeachersHeader();
+    researchClasses.verifyResourcesHeader();
+    researchClasses.getRemoveConcordConsortiumTeachersCheckbox().should("exist");
+    researchClasses.verifyCheckboxText();
+    researchClasses.verifyFooterNoteText();
+  });
+  it("Verify research classes dropdown", () => {
+    researchClasses.clickDropDown("Cohorts");
+    researchClasses.getCohortsDropDown().should("not.contain", "Test Project: Test Cohort");
+    researchClasses.getCohortsDropDown().contains("Test Project For Researcher: Test Cohort");
+    researchClasses.clickDropDown("Teachers");
+    researchClasses.getTeachersDropDown().contains("Cypress AutomationTeacher3 (cautomationTeacher3)");
+    researchClasses.clickDropDown("Resources");
+    researchClasses.getResourcesDropDown().contains("AKSims Snapshot Test Activity");
+  });
+  it("Verify remove concord consortium teachers checkbox", () => {
+    researchClasses.clickDropDown("Teachers");
+    researchClasses.getTeachersDropDown().contains("Sara Admin (admin_sara)");
+    researchClasses.getRemoveConcordConsortiumTeachersCheckbox().click({ force: true });
+    researchClasses.clickDropDown("Teachers");
+    researchClasses.getTeachersDropDown().should("not.contain", "Sara Admin (admin_sara)");
+  });
+  it("Verify results table", () => {
+    researchClasses.getResultsTable().should("not.exist");
+    researchClasses.clickDropDown("Cohorts");
+    researchClasses.getCohortsDropDown().contains("Test Project For Researcher: Test Cohort").click({ force: true });
+    researchClasses.getResultsTable().should("exist");
+    researchClasses.verifyResultsLabel();
+    researchClasses.getShowSchoolNameCheckbox().parent().contains("Show School Name");
+    researchClasses.verifyResultsTableHeader();
+    researchClasses.verifyResultsTableHeaderSortIcon("Cohort");
+    researchClasses.verifyResultsTableHeaderSortIcon("Teacher");
+    researchClasses.verifyResultsTableHeaderSortIcon("Class");
+    researchClasses.getShowSchoolNameCheckbox().click({ force: true });
+    researchClasses.getResultsTableSchoolColumn().should("contain", "School");
+    researchClasses.verifySchoolColumnSortIcon();
+    researchClasses.verifyConcordConsortiumSchoolNameNotDisplayed();
+    researchClasses.getRemoveConcordConsortiumTeachersCheckbox().click({ force: true });
+    researchClasses.verifyConcordConsortiumSchoolNameDisplayed();
+  });
+  it("Verify sorting", () => {
+    researchClasses.clickSortIcon(0);
+    researchClasses.verifyFirstRowData(0, "Test Cohort");
+    researchClasses.clickSortIcon(0);
+    researchClasses.verifyFirstRowData(0, "Test Cohort, Test Cohort For Researcher");
+    researchClasses.clickSortIcon(1);
+    researchClasses.verifyFirstRowData(1, "Cypress AutomationTeacher3");
+    researchClasses.clickSortIcon(1);
+    researchClasses.verifyFirstRowData(1, "Teacher School");
+    researchClasses.clickSortIcon(3);
+    researchClasses.verifyFirstRowData(3, "Concord Consortium");
+    researchClasses.clickSortIcon(3);
+    researchClasses.verifyFirstRowData(3, "Cypress Automation Test School");
+  });
+  it("Verify Reset All", () => {
+    researchClasses.getResetAllButton().should("exist");
+    researchClasses.getResetAllButton().should("contain", "Reset All");
+    researchClasses.verifySummary();
+    researchClasses.getResetAllButton().click({ force: true });
+    researchClasses.verifySummaryNotDisplayed();
+    researchClasses.getResultsTable().should("not.exist");
+  });
+});
+
+context("Researcher classes landing page", () => {
+
+  before(function() {
+    cy.visit(c.LEARN_PORTAL_BASE_URL); // Visit LEARN Portal home page
+    cy.login(c.TEACHER3_USERNAME, c.TEACHER3_PASSWORD); // Login as teacher user
+    cy.visit(c.LEARN_PORTAL_BASE_URL + '/portal/classes/228/materials?researcher=true');
+  });
+
+  after(function() {
+    cy.logout();
+    cy.clearAllCookies();
+  });
+
+  it("Verify research project in sidebar", () => {
+    researchClasses.getAssignmentPage().should("exist");
+    researchClasses.verifyHeader();
+    researchClasses.getOfferingsTable().should("exist");
+    researchClasses.verifySortIconNotDisplayed();
+    researchClasses.verifyActiveCheckboxDisabled();
+    researchClasses.verifyLockedCheckboxDisabled();
+    researchClasses.clickShowDetails();
+    researchClasses.verifyOfferingButtons();
+    researchClasses.verifyAnonymousStudentName();
+  });
+});
+
+context("Project Expiration Date", () => {
+
+  before(function() {
+    cy.visit(c.LEARN_PORTAL_BASE_URL); // Visit LEARN Portal home page
+    cy.login(c.TEACHER3_USERNAME, c.TEACHER3_PASSWORD); // Login as teacher user
+    cy.visit(c.LEARN_PORTAL_BASE_URL + '/users/78/edit');
+  });
+
+  after(function() {
+    cy.logout();
+    cy.clearAllCookies();
+  });
+
+  it("Verify project expiration date", () => {
+    researchClasses.getResearcherProjectChecbox().click({ force: true });
+    researchClasses.verifyDateInputNotShown();
+    researchClasses.getResearcherProjectChecbox().click({ force: true });
+    researchClasses.verifyDateInputShown();
+    researchClasses.getResearcherProjectDateInput().click({ force: true }).type("2024-04-01");
+    cy.wait(500);
+    researchClasses.getSaveButton().click({ force: true });;
+    researchClassesHelper.expandResearchProjects(); 
+    researchClassesHelper.getResearchProjectsOpen().should("exist");
+    researchClassesHelper.verifyProjectsNotDisplayed();
+    cy.visit(c.LEARN_PORTAL_BASE_URL + '/users/78/edit');
+    researchClasses.getResearcherProjectDateInput().click({ force: true }).clear();
+    cy.wait(500);
+    researchClasses.getSaveButton().click({ force: true });;
+    researchClassesHelper.expandResearchProjects();   
+    researchClassesHelper.getResearchProjectsOpen().should("exist");
+    researchClassesHelper.verifyProjectsDisplayed();
+  });
+});
---
+++ cypress/support/helpers/researcherClassesHelper.js
@@ -0,0 +1,31 @@
+import teacherHomePageElements from "../elements/teacher_home_page_elements.js";
+
+export function expandResearchProjects() {
+  cy.get(teacherHomePageElements.NAV_CLASSES).then(() => {
+    return cy.contains(teacherHomePageElements.LEFT_NAV_RESEARCH_PROJECTS, 'Research Projects');
+  }).then(($researchProjects) => {
+    if($researchProjects.find('> span[class^=\"closed\"]').length > 0) {
+      cy.contains(teacherHomePageElements.LEFT_NAV_RESEARCH_PROJECTS, 'Research Projects').click();
+    }
+  });
+}
+
+export function getResearchProjectsOpen() {
+  return cy.get(teacherHomePageElements.LEFT_NAV_RESEARCH_PROJECTS).contains('Research Projects').find('> span[class^=\"open\"]');
+}
+
+export function verifyProjectsDisplayed() {
+  return cy.get(teacherHomePageElements.LEFT_NAV_RESEARCH_PROJECTS).contains('Research Projects').find('ul').contains('Test Project For Researcher');
+}
+
+export function verifyProjectsNotDisplayed() {
+  return cy.get(teacherHomePageElements.LEFT_NAV_RESEARCH_PROJECTS).contains('Research Projects').find('ul').should("not.contain", "Test Project For Researcher");
+}
+
+export function clickProject() {
+  return cy.get(teacherHomePageElements.LEFT_NAV_RESEARCH_PROJECTS).contains('Research Projects').find('ul').contains('Test Project For Researcher').click();
+}
+
+export function getResearchClassLandingPage() {
+  return cy.get('#primary [class^=researcherClassesForm]');
+}
